6543bc7587c903ab8760447dc839d1c5d7e6b12c
holabayor <liasu.olabayo@gmail.com>
2023-02-19 21:30:23 +0100

    tasks

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,46 @@
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
+
+
+interface Director extends Teacher {
+    numberOfReports: number;
+}
+
+export interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+export const printTeacher: printTeacherFunction = (firstName: string, lastName: string) => {
+    return `${firstName.slice(0, 1)}. ${lastName}`
+}
+
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+
+export const StudentClass: StudentConstructor = class StudentClass implements StudentClassInterface {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
