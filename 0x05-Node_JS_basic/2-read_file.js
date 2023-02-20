4b8b94765aed1b02a3cb6e3dc0e75122f8d755a0
holabayor <liasu.olabayo@gmail.com>
2023-02-20 15:43:01 +0100

    task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,12 +1,31 @@
 const fs = require('fs');
 
+/**
+ * Counts the number of students in a file.
+ * @param {string} path - Path of the csv file
+ * @author Josh Liasu <a href="http://github.com/holabayor">
+ */
+
 const countStudents = (path) => {
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
+  try {
+    let data = fs.readFileSync(path)
+      .toString().split('\n');
+
+    console.log('Number of students: ', data.length - 1); // remove the header line
+    data = data.slice(1);
+    const subjects = {};
+    for (const line of data) {
+      const student = line.split(',');
+      if (!subjects[student[3]]) subjects[student[3]] = [];
+      subjects[student[3]].push(student[0]);
     }
-    console.log(data);
-  });
+    for (const subject in subjects) {
+      // if (subject)
+      console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 };
 
 module.exports = countStudents;
