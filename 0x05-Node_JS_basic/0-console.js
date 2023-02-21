ea61afe6285f3098ff32085809a762bfe218ade6
holabayor <liasu.olabayo@gmail.com>
2023-02-21 12:38:06 +0100

    task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,9 @@
+/**
+ * Displays a message to the STDOUT
+ * @param {string} message to display
+ * @author Josh Liasu <a href="http://github.com/holabayor">
+ */
+
 const displayMessage = (args) => {
   process.stdout.write(args + '\n');
 };
