35c3ffe866f012fb5c78a461dfc4e215b29cb0b6
holabayor <liasu.olabayo@gmail.com>
2023-02-22 10:48:39 +0100

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is : ${data}`);
+  if (data) process.stdout.write(`Your name is : ${data}`);
 });
 
 process.stdin.on('end', () => {
