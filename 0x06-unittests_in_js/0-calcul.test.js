810df5ec6efe5714c9d406c05f4ce9c4616e0913
holabayor <liasu.olabayo@gmail.com>
2023-02-23 13:38:17 +0100

    test file

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,19 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function () {
-  it('should return 10', function () {
-    assert.equal(calculateNumber(1.0, 3.0), 4);
+  it('should return 8', function () {
+    assert.strictEqual(calculateNumber(4.0, 3.6), 8);
+  });
+
+  it('should return 4', function () {
+    assert.strictEqual(calculateNumber(1.0, 2.7), 4);
+  });
+
+  it('should return 6', function () {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+
+  it('should return 2', function () {
+    assert.strictEqual(calculateNumber(-1.0, 2.7), 2);
   });
 });
