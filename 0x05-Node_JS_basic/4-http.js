7109a374d6ac85b2f10fd36db4e601198d9fdf2f
holabayor <liasu.olabayo@gmail.com>
2023-02-24 21:51:08 +0100

    eslint

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const http = require('http');
 const port = 1245;
 const host = 'localhost';
 
-const app = http.createServer(function (req, res) {
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
