25b7d08af7cc3514c1d58e014a06d89beba18802
holabayor <liasu.olabayo@gmail.com>
2023-02-24 21:48:46 +0100

    http

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,8 +6,8 @@ const host = 'localhost';
 const app = http.createServer(function (req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write('Hello Holberton School!');
-})
+  res.end('Hello Holberton School!');
+});
 
 app.listen(port, host, () => {
   console.log(`Server listening on port ${port}`);
