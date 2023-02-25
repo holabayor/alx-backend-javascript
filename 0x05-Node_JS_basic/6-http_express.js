ecb21375a0605a876edaf1ae29e005442736ec99
holabayor <liasu.olabayo@gmail.com>
2023-02-25 09:22:10 +0100

    sort

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,16 +1,15 @@
 const express = require('express');
+const app = express();
 
 const port = 1245;
 const host = 'localhost';
 
-const app = express();
-
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+    res.send('Hello Holberton School!');
 });
 
 app.listen(port, host, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
 
 module.exports = app;
