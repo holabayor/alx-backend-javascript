b305f7f8d3b9e145f20507e4432f7e07adbfd2fb
holabayor <liasu.olabayo@gmail.com>
2023-02-25 09:26:25 +0100

    eslint

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
 
@@ -7,6 +8,11 @@ const host = 'localhost';
 
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  res.send('This is the list of our students');
+  countStudents(process.argv[2]);
 });
 
 app.listen(port, host, () => {
