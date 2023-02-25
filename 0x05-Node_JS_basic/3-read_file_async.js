ee2a9cec53086d390ff421334ae4ab0d2a5bcc3e
holabayor <liasu.olabayo@gmail.com>
2023-02-25 11:16:53 +0100

    read file async

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -4,18 +4,20 @@
  * @author Josh Liasu <a href="http://github.com/holabayor">
  */
 const fs = require('fs');
+const { response } = require('./6-http_express');
 
 const countStudents = (path) => {
-  if (!fs.existsSync(path)) {
-    throw new Error('Cannot load the database');
-  }
   const promise = (resolve, reject) => {
-    fs.readFile(path, (error, data) => {
-      if (error || !fs.existsSync(path)) {
+    fs.readFile(path, 'utf8', (error, data) => {
+      if (error) {
         reject(Error('Cannot load the database'));
       }
+      let response = []
+      let msg = '';
       let content = data.toString().split('\n');
-      console.log(`Number of students: ${content.length - 1}`); // remove the header line
+      msg = `Number of students: ${content.length - 1}` // remove the header line
+      console.log(msg);
+      response.push(msg);
       content = content.slice(1);
       const subjects = {};
       for (const line of content) {
@@ -25,8 +27,10 @@ const countStudents = (path) => {
       }
       for (const subject in subjects) {
         if (subject) {
-          console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-          resolve();
+          msg = `Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`
+          console.log(msg);
+          response.push(msg);
+          resolve(response);
         }
       }
     });
