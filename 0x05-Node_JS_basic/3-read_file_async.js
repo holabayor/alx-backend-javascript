ef8dc9f375c6a01072cbf83bf863ad45b9e64f39
holabayor <liasu.olabayo@gmail.com>
2023-02-25 09:13:56 +0100

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,13 +6,16 @@
 const fs = require('fs');
 
 const countStudents = (path) => {
+  if (!fs.existsSync(path)) {
+    throw new Error('Cannot load the database');
+  }
   const promise = (resolve, reject) => {
     fs.readFile(path, (error, data) => {
-      if (error) {
+      if (error || !fs.existsSync(path)) {
         reject(Error('Cannot load the database'));
       }
       let content = data.toString().split('\n');
-      console.log('Number of students:', content.length - 1); // remove the header line
+      console.log(`Number of students: ${content.length - 1}`); // remove the header line
       content = content.slice(1);
       const subjects = {};
       for (const line of content) {
@@ -21,9 +24,10 @@ const countStudents = (path) => {
         subjects[student[3]].push(student[0]);
       }
       for (const subject in subjects) {
-      // if (subject)
-        console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
-        resolve();
+        if (subject) {
+          console.log(`Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`);
+          resolve();
+        }
       }
     });
   }; return new Promise(promise);
---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const port = 1245;
+const host = 'localhost';
+const database = process.argv.length > 2 ? process.argv[2] : '';
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+  if (url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    countStudents(database)
+      .then((data) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.write('This is the list of our students\n');
+        console.log(typeof data);
+        // process.stdout.write(data);
+        // res.write(Buffer.from(data));
+        res.end('Done loading');
+      });
+  }
+});
+
+app.listen(port, host, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
