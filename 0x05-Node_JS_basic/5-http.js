e72dca4a19bacbd3f9fdcc4f5924f122db8ffb3b
holabayor <liasu.olabayo@gmail.com>
2023-02-26 15:19:14 +0100

    task 5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -3,7 +3,7 @@ const countStudents = require('./3-read_file_async');
 
 const port = 1245;
 const host = 'localhost';
-const database = process.argv.length > 2 ? process.argv[2] : '';
+const database = process.argv.length > 2 ? process.argv[2] : 'database.csv';
 
 const app = http.createServer((req, res) => {
   const { url } = req;
@@ -17,10 +17,7 @@ const app = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.write('This is the list of our students\n');
-        res.write(data.join('\n'));
-        // process.stdout.write(data);
-        // res.write(Buffer.from(data));
-        res.end('\nDone loading');
+        res.end(data.join('\n'));
       });
   }
 });
