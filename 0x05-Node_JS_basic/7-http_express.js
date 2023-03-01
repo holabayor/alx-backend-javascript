ef1ef20540ceae0a88f2e0c9865f7c97f10f89fe
holabayor <liasu.olabayo@gmail.com>
2023-03-01 19:43:19 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -13,18 +13,22 @@ function countStudents(path) {
       if (error || !fs.existsSync(path)) {
         reject(Error('Cannot load the database'));
       }
-      const response = [];
-      let msg = '';
-      let content = data.toString().trim().split('\n');
-      msg = `Number of students: ${content.length - 1}`; // remove the header line
-      response.push(msg);
-      content = content.slice(1);
       const subjects = {};
-      for (const line of content) {
-        const student = line.split(',');
-        if (!subjects[student[3]]) subjects[student[3]] = [];
-        subjects[student[3]].push(student[0]);
+      let msg = '';
+      const response = [];
+      if (data) {
+        let content = data.toString().trim().split('\n');
+        msg = `Number of students: ${content.length - 1}`; // remove the header line
+        response.push(msg);
+        content = content.slice(1);
+
+        for (const line of content) {
+          const student = line.split(',');
+          if (!subjects[student[3]]) subjects[student[3]] = [];
+          subjects[student[3]].push(student[0]);
+        }
       }
+
       for (const subject in subjects) {
         if (subject) {
           msg = `Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`;
@@ -41,13 +45,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/students', (req, res) => {
-  countStudents(database).then((data) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write('This is the list of our students\n');
-    res.write(Buffer.from(data.join('\n')));
-    res.end();
-  });
+  countStudents(database)
+    .then((data) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/plain');
+      res.write('This is the list of our students\n');
+      res.write(Buffer.from(data.join('\n')));
+      res.end();
+    })
+    .catch((err) => {
+      res.status(200).write('This is the list of our students\n');
+      res.end(err.message);
+    });
 });
 
 app.listen(port, host, () => {
