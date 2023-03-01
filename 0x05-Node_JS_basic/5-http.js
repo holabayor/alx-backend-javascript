3965c86eb9df6ff978d85c8481be2163fd8bfc55
holabayor <liasu.olabayo@gmail.com>
2023-03-01 17:52:08 +0100

    eslint

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,9 +1,38 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const port = 1245;
 const host = 'localhost';
 const database = process.argv.length > 2 ? process.argv[2] : 'database.csv';
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, data) => {
+      if (error || !fs.existsSync(path)) {
+        reject(Error('Cannot load the database'));
+      }
+      const response = [];
+      let msg = '';
+      let content = data.toString().trim().split('\n');
+      msg = `Number of students: ${content.length - 1}`; // remove the header line
+      response.push(msg);
+      content = content.slice(1);
+      const subjects = {};
+      for (const line of content) {
+        const student = line.split(',');
+        if (!subjects[student[3]]) subjects[student[3]] = [];
+        subjects[student[3]].push(student[0]);
+      }
+      for (const subject in subjects) {
+        if (subject) {
+          msg = `Number of students in ${subject}: ${subjects[subject].length}. List: ${subjects[subject].join(', ')}`;
+          response.push(msg);
+        }
+      }
+      resolve(response);
+    });
+  });
+}
 
 const app = http.createServer((req, res) => {
   const { url } = req;
@@ -17,7 +46,8 @@ const app = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.write('This is the list of our students\n');
-        res.end(data.join('\n'));
+        res.write(Buffer.from(data.join('\n')));
+        res.end();
       });
   }
 });
